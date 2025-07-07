c2b1c14b664bbe59960d435ebbbd9f77cf29edd5
Oleg Portnov <drose1@list.ru>
2025-07-07 22:41:22 +0400

    upd api-instance.ts

--- frontendReactShop/src/entities/api/api-instance.ts
+++ frontendReactShop/src/entities/api/api-instance.ts
@@ -1,4 +1,8 @@
-const BASE_URL = 'http://localhost:3000';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = import.meta.env.VITE_API_URL || '';
 
 class ApiError extends Error {
   constructor(public response: Response) {

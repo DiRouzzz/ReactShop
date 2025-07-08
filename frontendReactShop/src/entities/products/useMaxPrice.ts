e6ab76aef0710b9f9b3dcda7013288978f1b9a4f
Oleg Portnov <drose1@list.ru>
2025-07-08 22:43:12 +0400

    upd useMaxPrice.ts

--- frontendReactShop/src/entities/products/useMaxPrice.ts
+++ frontendReactShop/src/entities/products/useMaxPrice.ts
@@ -4,7 +4,8 @@ import { jsonApiInstance } from '../api/api-instance';
 export const useMaxPrice = () => {
   return useQuery({
     queryKey: ['maxPrice'],
-    queryFn: () => jsonApiInstance<{ maxPrice: number }>('/products/max/price'),
+    queryFn: () =>
+      jsonApiInstance<{ maxPrice: number }>('/api/products/max/price'),
     select: (data) => data.maxPrice,
   });
 };
